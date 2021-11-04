7808ec11b2581f3fa9cf0e04949b062ddb94b78c
Mathew Bushuru <bushuruwafulamathew@gmail.com>
2021-11-04 03:04:49 -0700

    added views and html templates for the blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -23,5 +24,7 @@ class Post(models.Model):
     published = PublishedManager() #default custom manager for publsihe posts
     class Meta:
         ordering = ('-publish',)
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year,self.publish.month,self.publish.day,self.slug])
     def __str__(self):
         return self.title
